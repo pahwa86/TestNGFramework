195d62f2c36b448ebe3aba7b17ee84adc9a3f30a
Anoop Pahwa <pahwa86@yahoo.co.in>
2020-02-24 11:40:05 +0530

    updated

--- TestngProject/src/test/java/pages/VideoPlayPage.java
+++ TestngProject/src/test/java/pages/VideoPlayPage.java
@@ -2,15 +2,8 @@ package pages;
 
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class VideoPlayPage {
 	
